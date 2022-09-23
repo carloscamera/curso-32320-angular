815b311bea31e66ebe582fc01e5daf31b135cc3a
Carlos Cámera <103585506+carloscamera@users.noreply.github.com>
2022-09-22 22:39:14 -0300

    Mi primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    ListaComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
